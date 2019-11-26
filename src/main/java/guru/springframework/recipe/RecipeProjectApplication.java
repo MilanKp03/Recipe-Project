fac84831bfac01e9e6568005fc88e7dfe243c378
MilanKp03 <40432786+MilanKp03@users.noreply.github.com>
2019-11-26 13:46:25 +0100

    commit 1.0

---
+++ src/main/java/guru/springframework/recipe/RecipeProjectApplication.java
@@ -0,0 +1,15 @@
+package guru.springframework.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeProjectApplication.class, args);
+        System.out.println("testing app 222222");
+
+    }
+
+}
